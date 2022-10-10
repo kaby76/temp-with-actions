f4332a8f036308ba36ae002e7c787b9b198ac046
Ken Domino <kenneth.domino@domemtech.com>
2022-10-10 06:31:44 -0400

    Fix.

---
+++ myreindex.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+ls -R > grammars.json
+git diff --quiet
+if [ "$?" == "1" ]; then
+  git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+  git config --local user.name "github-actions[bot]"
+  git add grammars.json
+  git commit -m "generated"
+fi
+
+exit 0
